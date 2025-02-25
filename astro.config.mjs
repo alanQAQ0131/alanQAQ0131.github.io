948f337e41f7e882aec64708bf2b91cddf99a144
alanQAQ0131 <1136832886@qq.com>
2025-02-25 21:43:33 +0800

    test2

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://alanqaq0131.github.io",
-  base: "/fuwari-blog",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(
